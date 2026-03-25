2f6941d95cead2a74e49f76895cb47d5e8c2448b
saksham-s1ngh <saksham249singh@gmail.com>
2026-03-25 10:31:27 -0400

    CH1L1(BootsSoln): Updated with Solution + refactor - Separated split delimiter function and code from `textnode.py` file into a separate `inline_markdown.py` file - Separated the inline_markdown tests into its own stand-alone file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -57,26 +57,6 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.props, {"src": "https://www.google.com", "alt": "Blank image"})
-
-    def test_middle_split(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(new_nodes, [
-            TextNode("This is text with a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(" word", TextType.TEXT),
-        ])
-
-    def test_empty_split(self):
-        node = TextNode("`starting with code` then normal text", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(
-            new_nodes,
-            [
-                TextNode("starting with code", TextType.CODE),
-                TextNode(" then normal text", TextType.TEXT),
-            ]
-        )
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -45,22 +45,3 @@ def text_node_to_html_node(text_node: TextNode):
     else:
         raise NameError("Incorrect text type name")
         
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(TextNode(node.text, node.text_type))
-            continue
-        split_nodes = node.text.split(delimiter)
-        if len(split_nodes) % 2 == 0:
-            raise Exception("Closing delimiter not found")
-        
-        for index, splits in enumerate(split_nodes):
-            if splits == "":
-                continue
-            if index % 2 == 0:
-                new_nodes.append(TextNode(splits, TextType.TEXT))
-            else:
-                new_nodes.append(TextNode(splits, text_type))
-    return new_nodes
-
