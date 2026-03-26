01d2f67b52c0b3906b88e936ed3af724e9126be7
saksham-s1ngh <saksham249singh@gmail.com>
2026-03-26 16:05:27 -0400

    CH3L4(MySoln): Image and Link extraction - added markdown extraction methodds for image and link. - added tests to test these methods as well.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -18,3 +19,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(splits, text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    markdown_text_and_img = re.findall(r"!\[(.*?)\]\(([^)\s]+)\)", text) 
+    return markdown_text_and_img
+
+def extract_markdown_links(text):
+    markdown_text_and_url = re.findall(r"\[(.*?)\]\(([^)\s]+)\)", text)
+    return markdown_text_and_url
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 
 from textnode import TextType, TextNode
@@ -105,8 +107,43 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_multiple_images(self):
+       matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        ) 
+       self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_images_no_alt_text(self):
+        matches = extract_markdown_images(
+            "This is text with an ![](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot.dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot.dev", "https://www.boot.dev")], matches)
+
+    def test_extract_markdown_multiple_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_extract_markdown_links_no_alt_text(self):
+        matches = extract_markdown_links(
+                    "This is text with a link [](https://www.boot.dev)"
+                )
+        self.assertListEqual([("", "https://www.boot.dev")], matches)
+
 
 if __name__ == "__main__":
     unittest.main()
-
 
