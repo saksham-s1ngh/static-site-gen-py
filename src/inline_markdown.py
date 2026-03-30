9544e6f1c500ff078b7cdc940b87bb7d34f41d52
saksham-s1ngh <saksham249singh@gmail.com>
2026-03-30 15:56:39 -0400

    CH3L5(BootSoln): Correct split methods + tests - I had added some unintended bugs related to splitting the textnodes and handling multiple nodes which I corrected after reading the solution. - Main bugs were: -- Non-TEXT node handling: We tried to process all nodes regardless of type (unnecessary since we should only process text nodes) -- Empty image list early exit: I was taking the node_text and creating a new TextNode at the end when encountering a text node with no images/links. This was similar to intended behaviour but wasn't retaining the original attributes (which happens by just appending the text node) -- Malformed markdown validation: If the .split() doesn't produce two sections, my solution would quietly output incorrect code rather than providing an informative error.
    - Also had incorrect tests configured since I had implemented the code wrong, and it gave an insight into the fact that since my code was processing all nodes, the unnecessary work made sure my code would pass the bad tests too.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -23,41 +23,52 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for node in old_nodes:
+        # skip processing non-text nodes (since they might already be processed)
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
         node_text = node.text
         extracted_image_list = extract_markdown_images(node_text)
-
+        # if there's no images, skip processing and append as-is
+        if len(extracted_image_list) == 0:
+            new_nodes.append(node)
+            continue
         for ext_image in extracted_image_list:
             sections = node_text.split(f"![{ext_image[0]}]({ext_image[1]})", 1)
-
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
             if sections[0]:
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            node_text = sections[1]
             new_nodes.append(TextNode(ext_image[0], TextType.IMAGE, ext_image[1]))
-
-        if node_text:
+            node_text = sections[1]
+        if node_text != "": # for any trailing text nodes after image nodes
             new_nodes.append(TextNode(node_text, TextType.TEXT))
-
     return new_nodes
 
 def split_nodes_link(old_nodes):
     new_nodes = []
     for node in old_nodes:
+        # skip processing non-text nodes (since they might already be processed)
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
         node_text = node.text
         extracted_url_list = extract_markdown_links(node_text)
-
+        # if there's no urls, skip processing and append as-is
+        if len(extracted_url_list) == 0:
+            new_nodes.append(node)
+            continue
         for ext_url in extracted_url_list:
             sections = node_text.split(f"[{ext_url[0]}]({ext_url[1]})", 1)
-
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
             if sections[0]:
                 new_nodes.append(TextNode(sections[0], TextType.TEXT))
             node_text = sections[1]
             new_nodes.append(TextNode(ext_url[0], TextType.LINK, ext_url[1]))
-
-        if node_text:
+        if node_text != "":
             new_nodes.append(TextNode(node_text, TextType.TEXT))
-
     return new_nodes
-
 
 def extract_markdown_images(text):
     markdown_text_and_img = re.findall(r"!\[(.*?)\]\(([^)\s]+)\)", text) 
